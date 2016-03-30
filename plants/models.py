1905ca54c8d1b29b1f40d1d295962c801a2ba3f5
Magnus Lund <mgnusl@gmail.com>
2016-03-30 09:35:40 +0200

    Update models. Make field choices strict

--- plants/models.py
+++ plants/models.py
@@ -8,23 +8,23 @@ class Plant(models.Model):
     # Humidity. LOW, MODERATE, HIGH
     ideal_humidity = models.CharField(max_length=100, choices=[('LOW', 'LOW'), ('MODERATE', 'MODERATE'), ('HIGH', 'HIGH')])
 
+    # How much sun the plant prefers. Not required.
+    sun_preference = models.CharField(max_length=100, blank=True, help_text="How much sun the plant prefers",
+                                      choices=[('MINIMAL', 'MINIMAL'), ('FULL', 'FULL')])
+
+    # How much shade the plant can tolerate. Not required.
+    shade_tolerance = models.CharField(max_length=100, blank=True, help_text="How much shade the plant can tolerate",
+                                       choices=[('NONE', 'NONE'), ('LIGHT', 'LIGHT'), ('PERMANENT', 'PERMANENT')])
+
     # How often the plant needs fertilizer. In days. Not required
     fertilizing_interval = models.IntegerField(default=0, blank=True,
                                                help_text="How often the plant needs fertilizer (in days)")
 
-    # How much sun the plant prefers on a scale from 0-2. Not required.
-    sun_preference = models.IntegerField(default=0, blank=True, help_text="How much sun does the plant prefer (0-2)")
-
-    # How much shade the plant can tolerate on a scale from 0-4. Not required.
-    shade_tolerance = models.IntegerField(default=0, blank=True, help_text="How much shade can the plant tolerate (0-4)")
-
     # Not used for now
-    ideal_ph_min = models.DecimalField(max_digits=3, decimal_places=2, default=0, blank=True,
+    ideal_ph_min = models.DecimalField(max_digits=3, decimal_places=2, default=-1, blank=True,
                                        help_text="Minimum ideal pH value")
-    ideal_ph_max = models.DecimalField(max_digits=3, decimal_places=2, default=0, blank=True,
+    ideal_ph_max = models.DecimalField(max_digits=3, decimal_places=2, default=-1, blank=True,
                                        help_text="Maximum ideal pH value")
-
-    # image = models.ImageField(...)
 
     def __str__(self):
         return self.name
