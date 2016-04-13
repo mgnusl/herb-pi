53672d3c4722413358a29f7e3b9a6984e534ae27
Thomas Gjerde <thomasgjerde90@gmail.com>
2016-04-13 11:13:16 +0200

    Adds plant type index and changes urls

--- plants/urls.py
+++ plants/urls.py
@@ -10,13 +10,13 @@ urlpatterns = [
     url(r'^api/wateringlog/(?P<fk>[0-9]+)/$', views.watering_log),
     url(r'^api/plantinstance/(?P<pk>[0-9]+)/$', views.plant_instance),
     url(r'^api/plantinstances/$', views.plantinstance_list),
-    url(r'^plants/$', views.plants_index, name='plants/index'),
-    url(r'^instances/$', views.plant_instances_index, name='instances/index'),
-    url(r'^instances/new/$', views.new_plant_instance, name='instances/new'),
-    url(r'^instances/edit/(?P<id>\w+)$', views.new_plant_instance, name='instances/edit'),
-    url(r'^instances/calibrate/(?P<plant_instance_id>\w+)$', views.calibrate_sensor, name='calibrate'),
+    url(r'^plant_types/$', views.plants_index, name='plants/index'),
+    url(r'^plants/$', views.plant_instances_index, name='instances/index'),
+    url(r'^plants/new/$', views.new_plant_instance, name='instances/new'),
+    url(r'^plants/edit/(?P<id>\w+)$', views.new_plant_instance, name='instances/edit'),
+    url(r'^plants/calibrate/(?P<plant_instance_id>\w+)$', views.calibrate_sensor, name='calibrate'),
 
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', views.plant_instances_index),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
